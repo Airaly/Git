4e7e13c6282216597a9fef6ccbde4bebee1a194f
Airaly <363782125@qq.com>
2015-10-25 17:48:16 +0800

    Revert "second changed"
    This reverts commit c0e304882d0bd07f683d6a29d883fdd12b2f6cfd.

--- SecondGUI.java
+++ SecondGUI.java
@@ -1,54 +1,34 @@
-package com.briup.thread;
+package com.briup.gui;
 
-public class Account {
-	private long accountId;
-	private double balance;
-	public Account(long accountId,double balance){
-		this.accountId = accountId;
-		this.balance = balance;
-	}
-	public long getAccountId() {
-		return accountId;
-	}
-	public double getBalance() {
-		return balance;
-	}
-	//取钱
-	/*synchronized*/ public double withdraw(double atm){
-			if(atm<=0){
-				return 0.0;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class SecondGUI {
+	public static void main(String[] args) {
+		//1.选择容器
+		JFrame frame = new JFrame("SecondGUI");
+//		2.选择布局管理器
+		System.out.println(frame.getLayout());
+//		3.添加组件
+		JButton btn = new JButton("btn 2");
+		ImageIcon icon = new ImageIcon("1.jpg");
+		Image temp = icon.getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT);
+		btn.setIcon(new ImageIcon(temp));
+		frame.add(btn);
+//		4.添加事件处理
+		btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("btn click...");
 			}
-		synchronized (this) {
-			double temp = this.balance;
-			if(atm>temp){
-				return 0.0;
-			}
-			temp-=atm;
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			this.balance = temp;
-		}
-			return atm;
-	}
-	//存钱
-	/*synchronized*/ public double deposit(double atm){
-		if(atm<=0){
-			return 0.0;
-		}
-		synchronized (this) {
-			double temp = this.balance;
-			temp+=atm;
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			this.balance = temp;
-		}
-		return atm;
+		});
+		frame.setBounds(200, 100, 300, 300);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
 
